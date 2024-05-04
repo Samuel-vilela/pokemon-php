af4219d2330d14fd3623f32aeb349363057b3641
Samuel-vilela <146647141+Samuel-vilela@users.noreply.github.com>
2024-05-03 22:24:15 -0300

    Pokedex

---
+++ admin.php
@@ -0,0 +1,8 @@
+<?PHP include"cabecalho.php"; ?>
+
+<h2>Ambiente administrativo </h2>
+<a href="form-inserir.php" class="btn btn-primary">NOVO POKEMON</a>
+
+
+
+<?php include "rodape.php"; ?>
---
+++ form-inserir.php
@@ -0,0 +1,17 @@
+<?PHP include "cabecalho.php"; ?> 
+
+<h2>CADASTRAR UM NOVO POKEMON</h2>
+<form  method="post" action="salvar.php" enctype="multipart/form-data">
+    <input type="text" name="nome" placeholder="nome" class="form-control">
+    <input type="radio" name="tipo" value="Normal" class="form-check-input"> Normal
+    <input type="radio" name="tipo" value="Fogo"class="form-check-input"> Fogo
+    <input type="radio" name="tipo" value="Água"class="form-check-input"> Agua
+    <input type="radio" name="tipo" value="Grama"class="form-check-input"> Grama
+    <input type="radio" name="tipo" value="Voador"class="form-check-input"> Voador
+    <br>
+    foto: <input type="file" name="foto" class="form-control">
+    <br>
+    <button class="btn btn-primary" type="submit">SALVAR POKEMON</button>
+</form>
+
+<?PHP include "rodape.php"; ?>
